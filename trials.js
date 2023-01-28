ec434f4e2bd61c28afc5db90aae8042051a617fe
Newton Ombese <newtonombese1@gmail.com>
2023-01-28 07:46:29 +0300

    Code challenge cats and a mouse completed and tested on the hackerrank editor and all test cases passed

--- trials.js
+++ trials.js
@@ -1,4 +1,22 @@
+'use strict';
+
 // Newton's solution
+// Cats and A mouse solution
+function catAndMouse(x,y,z){
+    let catAPosition = Math.abs(z - x);
+    let catBPosition = Math.abs(z - y);
+
+    if(catAPosition < catBPosition){
+        return "Cat A";
+    }
+    else if(catBPosition < catAPosition){
+        return "Cat B";
+    }
+    else{
+        return "Mouse C";
+    }
+}
+
 
 
 // Abraham's solution
