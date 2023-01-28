f2e52814f092ebd74e9ce1838fa3d7a6eec91591
gbayiola <olowogbafame@gmail.com>
2023-01-28 05:28:48 +0100

    olowogbafame@gmail.com

--- trials.js
+++ trials.js
@@ -20,3 +20,13 @@ function catAndMouse(x,y,z){
 
 
 // Abraham's solution
+
+let num = "";
+let count = 0;
+num = n.toString();
+for (let i = 0; i < num.length; i++){
+    if (num[i] != 0 && n % num[i] ===0){
+        count +=1;
+    }
+}
+return count
